74bee4bbbe7a29f4f57543fcc2a7a5f9fab752cf
Prithvi Pradhan <97850500+PrithviPradhan@users.noreply.github.com>
2022-10-20 15:27:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+ public static void main(String[] args) {
+   addTwos();
+   greet();
+   
+ }
+    public static void addTwos(){
+      int x = 2+2;
+   System.out.println(+ x);
+
+}
+  
+  public static void greet(){
+    System.out.println("Hello,how are you?");
+
+ }
+}   
+   
+  
